ee266bb158db402a749709a1b812235debebbdfa
hồ trọng hoàng <tronghoang1308@gmail.com>
2021-12-24 20:18:46 +0700

    bai tap class 3

---
+++ bai1 class3.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+	int a;
+	printf("Nhap a=");
+	scanf("%d",&a);
+	if (2<=a<=8){
+		if (a==2);{
+		printf("day la thu 2");
+		}else (a==3);{
+		printf("day la thu 3");
+		}else (a==4);{
+		printf("day la thu 4");
+		}else (a==5);{
+		printf("day la thu 5");
+		}else (a==6);{
+		printf("day la thu 6");
+		}else (a==7);{
+		printf("day la thu 7");
+		}else (a==8);{
+		printf("day la chu nhat");
+	}else{
+		printf(khong phai ngay trong tuan);
+	}
+	}
+}
